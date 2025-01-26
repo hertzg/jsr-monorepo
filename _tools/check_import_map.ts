e96543b796992f9fb6c2aeb5e1634f8c8d378a90
Giorgi Kotchlamazashvili <1886698+hertzg@users.noreply.github.com>
2025-01-26 22:36:28 +0400

    chore(*): use import_map.json when bumping

---
+++ _tools/check_import_map.ts
@@ -0,0 +1,39 @@
+import denoJson from "../deno.json" with { type: "json" };
+import importMap from "../import_map.json" with { type: "json" };
+import { join } from "jsr:@std/path";
+
+// deno-lint-ignore no-explicit-any
+const imports = importMap.imports as any;
+const denoJsonList = Promise.all(
+  denoJson.workspace.map((w) =>
+    Deno.readTextFile(join(w, "deno.json")).then(JSON.parse)
+  ),
+);
+
+let failed = false;
+
+for (const denoJson of await denoJsonList) {
+  const dependency = imports[denoJson.name];
+
+  if (!dependency) {
+    console.warn(`check_import_map: No import map entry found for ${denoJson.name}`);
+    failed = true;
+    continue;
+  }
+  const correctDependency = `jsr:${denoJson.name}@^${denoJson.version}`;
+  if (dependency !== correctDependency) {
+    console.warn(
+      `check_import_map: Invalid import map entry for ${denoJson.name}: ${dependency}`,
+    );
+    console.warn(
+      `check_import_map: Expected: ${correctDependency}`,
+    );
+    failed = true;
+  }
+}
+
+if (failed) {
+  Deno.exit(1);
+}
+
+console.log("check_import_map: ok");
